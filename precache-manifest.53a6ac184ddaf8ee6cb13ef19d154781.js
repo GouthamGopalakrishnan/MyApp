816397e24b430e2a31574f91f5f3f09fb05d4d4b
Goutham G <gauthamg1999@gmail.com>
2020-07-02 00:33:39 +0530

    Updates

---
+++ precache-manifest.53a6ac184ddaf8ee6cb13ef19d154781.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5723728ba8ba44c78109f98c4a310792",
+    "url": "/MyApp/index.html"
+  },
+  {
+    "revision": "c89efdb82846ff4ef122",
+    "url": "/MyApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/MyApp/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyApp/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c89efdb82846ff4ef122",
+    "url": "/MyApp/static/js/main.dfb9f41b.chunk.js"
+  },
+  {
+    "revision": "7863df859e2bb77542e1",
+    "url": "/MyApp/static/js/runtime-main.59b1f770.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MyApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
